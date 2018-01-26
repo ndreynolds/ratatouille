92845112d68f3f51744e4a15f36399ceb66993c6
Nick Reynolds <ndreynolds@posteo.de>
2018-01-26 21:23:37 +0100

    Adds missing files to package config

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule ExTermbox.Mixfile do
 
   defp package do
     [
-      files: ~w(lib priv mix.exs README.md LICENSE),
+      files: ~w(c_src lib priv Makefile mix.exs README.md LICENSE),
       maintainers: ["Nick Reynolds"],
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/ndreynolds/ex_termbox"}
