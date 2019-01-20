e9b6b08fef6a6b4586db64169dd9cb53e72df411
Nick Reynolds <ndreynolds@posteo.de>
2019-01-20 23:44:02 +0100

    Bumps version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ratatouille.Mixfile do
   def project do
     [
       app: :ratatouille,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
