cc92bb1ec27773108092766009744b4ac74a778b
Nick Reynolds <ndreynolds@posteo.de>
2019-01-20 23:02:14 +0100

    Points constants delegations at ex_termbox docs

--- lib/ratatouille/constants.ex
+++ lib/ratatouille/constants.ex
@@ -5,24 +5,25 @@ defmodule Ratatouille.Constants do
 
   alias ExTermbox.Constants, as: TermboxConstants
 
-  defdelegate keys, to: TermboxConstants
-  defdelegate key(name), to: TermboxConstants
+  @constant_types [
+    {:key, :keys},
+    {:color, :colors},
+    {:attribute, :attributes},
+    {:event_type, :event_types},
+    {:error_code, :error_codes},
+    {:input_mode, :input_modes},
+    {:output_mode, :output_modes}
+  ]
 
-  defdelegate colors, to: TermboxConstants
-  defdelegate color(name), to: TermboxConstants
+  for {lookup, collection} <- @constant_types do
+    @doc """
+    See `ExTermbox.Constants.#{lookup}/1`.
+    """
+    defdelegate unquote(lookup)(name), to: TermboxConstants
 
-  defdelegate attributes, to: TermboxConstants
-  defdelegate attribute(name), to: TermboxConstants
-
-  defdelegate event_types, to: TermboxConstants
-  defdelegate event_type(name), to: TermboxConstants
-
-  defdelegate error_codes, to: TermboxConstants
-  defdelegate error_code(name), to: TermboxConstants
-
-  defdelegate input_modes, to: TermboxConstants
-  defdelegate input_mode(name), to: TermboxConstants
-
-  defdelegate output_modes, to: TermboxConstants
-  defdelegate output_mode(name), to: TermboxConstants
+    @doc """
+    See `ExTermbox.Constants.#{collection}/1`.
+    """
+    defdelegate unquote(collection)(), to: TermboxConstants
+  end
 end
