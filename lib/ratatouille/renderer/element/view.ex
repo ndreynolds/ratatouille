87f135742c0e6084e5fbf828830ee2c1b3a70880
Nick Reynolds <ndreynolds@posteo.de>
2019-02-23 18:53:27 +0100

    Render view's bottom bar after child content
    Small fix that ensures that any overflowing content can't overwrite the
    bar. We generally want the opposite to happen so that whatever is in the
    bottom bar (e.g., status, navigation) is always visible.

--- lib/ratatouille/renderer/element/view.ex
+++ lib/ratatouille/renderer/element/view.ex
@@ -10,8 +10,8 @@ defmodule Ratatouille.Renderer.Element.View do
   def render(canvas, %Element{attributes: attrs, children: children}, render_fn) do
     canvas
     |> render_top_bar(attrs[:top_bar], render_fn)
-    |> render_bottom_bar(attrs[:bottom_bar], render_fn)
     |> render_fn.(children)
+    |> render_bottom_bar(attrs[:bottom_bar], render_fn)
   end
 
   defp render_top_bar(canvas, nil, _render_fn), do: canvas
