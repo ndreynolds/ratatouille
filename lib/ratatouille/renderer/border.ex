0559718cbcf91291226d63b55d33bf306331ec08
Dylan Hoefsloot <dylan.hoefsloot@outlook.com>
2021-10-04 19:32:45 +1300

    Add support for coloured borders

--- lib/ratatouille/renderer/border.ex
+++ lib/ratatouille/renderer/border.ex
@@ -16,18 +16,18 @@ defmodule Ratatouille.Renderer.Border do
     top_left: "┌"
   }
 
-  def render(%Canvas{render_box: box} = canvas) do
+  def render(%Canvas{render_box: box} = canvas, attrs) do
     width = Box.width(box)
     height = Box.height(box)
 
     canvas
-    |> Line.render_horizontal(Box.top_left(box), @borders.top, width)
-    |> Line.render_horizontal(Box.bottom_left(box), @borders.bottom, width)
-    |> Line.render_vertical(Box.top_left(box), @borders.left, height)
-    |> Line.render_vertical(Box.top_right(box), @borders.right, height)
-    |> Text.render(Box.top_left(box), @borders.top_left)
-    |> Text.render(Box.top_right(box), @borders.top_right)
-    |> Text.render(Box.bottom_left(box), @borders.bottom_left)
-    |> Text.render(Box.bottom_right(box), @borders.bottom_right)
+    |> Line.render_horizontal(Box.top_left(box), @borders.top, width, attrs)
+    |> Line.render_horizontal(Box.bottom_left(box), @borders.bottom, width, attrs)
+    |> Line.render_vertical(Box.top_left(box), @borders.left, height, attrs)
+    |> Line.render_vertical(Box.top_right(box), @borders.right, height, attrs)
+    |> Text.render(Box.top_left(box), @borders.top_left, attrs)
+    |> Text.render(Box.top_right(box), @borders.top_right, attrs)
+    |> Text.render(Box.bottom_left(box), @borders.bottom_left, attrs)
+    |> Text.render(Box.bottom_right(box), @borders.bottom_right, attrs)
   end
 end
